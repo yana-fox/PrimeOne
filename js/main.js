a76196c1fc9e1c4460704fa2b8d991dab841bd15
Yana <fuksinayana@gmail.com>
2020-08-12 15:37:14 +0300

    Добавлены стили для блока Private

--- js/main.js
+++ js/main.js
@@ -48,3 +48,12 @@ $('.header-menu__icon').click(function(event){
 		$('body,html').scrollTop(parseInt($('body').data('scroll')));
 	}
 });
+
+function ibg(){
+	$.each($('.ibg'), function(index, val){
+		if($(this).find('img').length>0){
+			$(this).css('background-image', 'url("'+$(this).find('img').attr('src')+'")');
+		}
+	});
+}
+ibg();
